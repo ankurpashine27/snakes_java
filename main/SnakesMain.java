f53927c580db23939b59b9dde41cd5a652174113
Ankur Pashine <ankurpashine27@gmail.com>
2025-07-07 09:11:11 +0530

    Initial

--- main/SnakesMain.java
+++ main/SnakesMain.java
@@ -1,7 +1,12 @@
-package main;
 
-public class SnakesMain {
+import javax.swing.JFrame;
+
+class SnakesMain {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Snakes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
